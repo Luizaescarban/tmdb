705e784b80346a2849c9a9eb3690d2de1615e4e2
Luizaescarban <luizaescarbandasil@gmail.com>
2024-12-10 20:37:14 -0300

    progresso no site

---
+++ src/views/TvView.vue
@@ -0,0 +1,107 @@
+<script setup>
+  import { ref, onMounted } from 'vue';
+  import api from '@/plugins/axios';
+
+  const genres = ref([]);
+
+  onMounted(async () => {
+    const response = await api.get('genre/tv/list?language=pt-BR');
+    genres.value = response.data.genres;
+  });
+
+  const tvs = ref([]);
+
+  const listTvs = async (genreId) => {
+    const response = await api.get('discover/tv', {
+        params: {
+            with_genres: genreId,
+            language: 'pt-BR'
+        }
+    });
+    tvs.value = response.data.results
+};
+</script>
+
+<template>
+  <h1>Filmes</h1>
+  <ul class="genre-list">
+    <li
+    v-for="genre in genres"
+    :key="genre.id"
+    @click="listTvs(genre.id)"
+    class="genre-item"
+  >
+    {{ genre.name }}
+  </li>
+  </ul>
+  <div class="tv-list">
+  <div v-for="tv in tvs" :key="tv.id" class="tv-card">
+
+    <img
+      :src="`https://image.tmdb.org/t/p/w500${tv.poster_path}`"
+      :alt="tv.title"
+    />
+    <div class="tv-details">
+      <p class="tv-title">{{ tv.title }}</p>
+      <p class="tv-release-date">{{ tv.release_date }}</p>
+      <p class="tv-genres">{{ tv.genre_ids }}</p>
+    </div>
+
+  </div>
+</div>
+</template>
+
+<style scoped>
+.genre-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  margin-bottom: 2rem;
+}
+
+.genre-item {
+  background-color: #387250;
+  border-radius: 1rem;
+  padding: 0.5rem 1rem;
+  color: #fff;
+}
+
+.genre-item:hover {
+  cursor: pointer;
+  background-color: #4e9e5f;
+  box-shadow: 0 0 0.5rem #387250;
+}
+.tv-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.tv-card {
+  width: 15rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tv-card img {
+  width: 100%;
+  height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tv-details {
+  padding: 0 0.5rem;
+}
+
+.tv-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
+}
+</style>
