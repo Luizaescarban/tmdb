feb37f37da09416c350d2eaeeba599cfcfcd237e
Luizaescarban <luizaescarbandasil@gmail.com>
2024-12-11 11:18:58 -0300

    termino da primeira parte

--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -1,54 +1,85 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
-  import api from '@/plugins/axios';
+import { ref, onMounted } from 'vue'
+import api from '@/plugins/axios'
+import Loading from 'vue-loading-overlay'
+import { useGenreStore } from '@/stores/genre'
+import { useRouter } from 'vue-router'
+const router = useRouter()
 
-  const genres = ref([]);
 
-  onMounted(async () => {
-    const response = await api.get('genre/tv/list?language=pt-BR');
-    genres.value = response.data.genres;
+function openTV(tvId) {
+  router.push({ name: 'TVDetails', params: { tvId } });
+}
+const genreStore = useGenreStore()
+
+// function getGenreName(id) {
+//   const genero = genres.value.find((genre) => genre.id === id)
+//   return genero.name
+// }
+
+const isLoading = ref(false)
+
+// const genres = ref([])
+
+onMounted(async () => {
+  isLoading.value = true
+  await genreStore.getAllGenres('tv')
+  isLoading.value = false
+})
+
+const tvs = ref([])
+
+const listTvs = async (genreId) => {
+  genreStore.setCurrentGenreId(genreId);
+  isLoading.value = true;
+  const response = await api.get('discover/tv', {
+    params: {
+      with_genres: genreId,
+      language: 'pt-BR',
+    },
   });
-
-  const tvs = ref([]);
-
-  const listTvs = async (genreId) => {
-    const response = await api.get('discover/tv', {
-        params: {
-            with_genres: genreId,
-            language: 'pt-BR'
-        }
-    });
-    tvs.value = response.data.results
+  tvs.value = response.data.results;
+  isLoading.value = false;
 };
+const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR')
 </script>
 
 <template>
-  <h1>Filmes</h1>
+  <h1>Programas de Tv</h1>
   <ul class="genre-list">
     <li
-    v-for="genre in genres"
-    :key="genre.id"
-    @click="listTvs(genre.id)"
-    class="genre-item"
-  >
-    {{ genre.name }}
-  </li>
+      v-for="genre in genreStore.genres"
+      :key="genre.id"
+      @click="listTvs(genre.id)"
+      class="genre-item"
+    >
+      {{ genre.name }}
+    </li>
   </ul>
+  <loading v-model:active="isLoading" is-full-page />
   <div class="tv-list">
-  <div v-for="tv in tvs" :key="tv.id" class="tv-card">
-
-    <img
-      :src="`https://image.tmdb.org/t/p/w500${tv.poster_path}`"
-      :alt="tv.title"
-    />
-    <div class="tv-details">
-      <p class="tv-title">{{ tv.title }}</p>
-      <p class="tv-release-date">{{ tv.release_date }}</p>
-      <p class="tv-genres">{{ tv.genre_ids }}</p>
+    <div v-for="tv in tvs" :key="tv.id" class="tv-card">
+      <img
+  :src="`https://image.tmdb.org/t/p/w500${tv.poster_path}`"
+  :alt="tv.title"
+  @click="openTV(tv.id)"
+/>
+      <div class="tv-details">
+        <p class="tv-title">{{ tv.title }}</p>
+        <p class="tv-release-date">{{ formatDate(tv.release_date) }}</p>
+        <p class="tv-genres">
+          <span
+            v-for="genre_id in tv.genre_ids"
+            :key="genre_id"
+            @click="listTvs(genre_id)"
+            :class="{ active: genre_id === genreStore.currentGenreId }"
+          >
+            {{ genreStore.getGenreName(genre_id) }}
+          </span>
+        </p>
+      </div>
     </div>
-
   </div>
-</div>
 </template>
 
 <style scoped>
@@ -103,5 +134,38 @@
   font-weight: bold;
   line-height: 1.3rem;
   height: 3.2rem;
+}
+.tv-genres {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.2rem;
+}
+
+.tv-genres span {
+  background-color: #748708;
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  color: #fff;
+  font-size: 0.8rem;
+  font-weight: bold;
+}
+
+.tv-genres span:hover {
+  cursor: pointer;
+  background-color: #455a08;
+  box-shadow: 0 0 0.5rem #748708;
+}
+.active {
+  background-color: #67b086;
+  font-weight: bolder;
+}
+
+.tv-genres span.active {
+  background-color: #abc322;
+  color: #000;
+  font-weight: bolder;
 }
 </style>
